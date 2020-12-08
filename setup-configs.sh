7216904d63e41f489137664ddaff064ff3251b0a
gamerhat <pranavkulkarni195@gmail.com>
2020-12-08 15:59:38 +0530

    Improved install script

--- setup-configs.sh
+++ setup-configs.sh
@@ -1,3 +1,21 @@
+hashes="################################################################################"
+
+toilet -kf circle -F gay $hashes
+echo ""
+
+toilet -kf slant -F gay   gamerhat
+
+echo "$(date '+%D %T' | toilet -kf term -F border --gay)"
+
+echo ""
+
+toilet -kf term --gay This was designed in the late-afternoon of 08th December, 2020. 
+
+echo ""
+
+toilet -kf circle -F gay $hashes
+
+
 cp -r .config/* ~/.config/
 cp .* ~/
 cp -r etc/lightdm/themes/* /etc/lightdm/themes/*
